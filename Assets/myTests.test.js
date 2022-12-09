39279544c6a50d7e6a4c977c7b30174f5a7a30a4
Gilbert Malova <malovagilbert@gmail.com>
2022-12-09 16:53:27 +0300

    Added calcul\ator test

--- Assets/myTests.test.js
+++ Assets/myTests.test.js
@@ -1,6 +1,7 @@
 const capitalize = require('./capitalize');
 const sum = require('./sum');
 const reverseString = require('./reverse');
+const calculator = require('./calculator');
 
 test('Is first letter capitalized', () => {
   expect(capitalize('string')).toBe('String')
@@ -13,3 +14,10 @@ test('adds 1 + 2 to equal 3', () => {
 test('is string reversed', () => {
   expect(reverseString('string')).toBe('gnirts')
 });
+
+test('calculator', () => {
+  expect(calculator.add(2, 2)).toEqual(4);
+  expect(calculator.subtract(2, 1)).toEqual(1);
+  expect(calculator.multiply(2, 2)).toEqual(4);
+  expect(calculator.divide(3, 2)).toBeCloseTo(1.5);
+})
